9b78f4eb30fa3c25a311755a70c61e8eefc45e3b
Humberto Torres <133621997+H05T3@users.noreply.github.com>
2025-01-17 15:50:42 +0000

    worked on guesser

--- guesserGame.c
+++ guesserGame.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+#define TRUE -1
+#define FALSE 0
 int main(){
+	char playerName[20];
+	printf("What is your name?:");
+	scanf("%s",playerName);
+	printf("Hello, %s!\n",playerName);
 	
-	
-	
-	return 0;
+	int randomVar;
+	int maxGuess = 3;
+	int userGuess;
+	int KEEPGOING = TRUE;
+
+	while(KEEPGOING){
+		printf("Enter your guess: ");
+		scanf("%d",userGuess);
+		if(userGuess == randomVar){
+			printf("Correct! The random number was %d \n",randomVar);
+			KEEPGOING = FALSE;
+		}
+
+	}
+
+	return (0);
 }
